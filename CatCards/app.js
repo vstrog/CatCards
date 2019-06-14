2c79067bfc3b62d17e3046ab324a430ba8e36ca2
vstrog <39245801+vstrog@users.noreply.github.com>
2019-06-15 02:48:16 +0300

    Add files via upload

---
+++ CatCards/app.js
@@ -0,0 +1,32 @@
+const heading = 'Please Meet Our Cats';
+let i = 0;
+
+const typing = () => {
+    if(i < heading.length) {
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+
+        setTimeout(typing, 150);
+    }
+}
+
+typing();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
